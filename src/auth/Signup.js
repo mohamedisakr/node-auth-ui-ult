e49a9c64f19ef2a2a5a8c8cdea0b142a553f6719
mohamedisakr <mohamedisakr@gmail.com>
2022-02-17 10:54:18 +0200

    implementing signup form basic layout

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../core/Layout'
 
 const Signup = () => {
+  const [name, setName] = useState('test man')
+  const [email, setEmail] = useState('testman1978@hotmail.com')
+  const [password, setPassword] = useState('Password123')
+  const [buttonText, setButtonText] = useState('Submit')
+
+  const handleNameChange = (e) => {
+    e.preventDefault()
+    setName(e.target.value)
+  }
+
+  const handleEmailChange = (e) => {
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault()
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const signupForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Name:</label>
+          <input
+            value={name}
+            onChange={handleNameChange}
+            type="text"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Email:</label>
+          <input
+            value={email}
+            onChange={handleEmailChange}
+            type="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Password:</label>
+          <input
+            value={password}
+            onChange={handlePasswordChange}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        <div>
+          <button onClick={handleSubmit} className="btn btn-primary">
+            {buttonText}
+          </button>
+        </div>
+      </form>
+    )
+  }
+
   return (
     <Layout>
-      <h1>Signup</h1>
+      <div className="col-d-6 offset-md-3">
+        <h1 className="p-5 text-center">Signup</h1>
+        {signupForm()}
+        {/* <pre>{JSON.stringify({name, email, password})}</pre> */}
+      </div>
     </Layout>
   )
 }
