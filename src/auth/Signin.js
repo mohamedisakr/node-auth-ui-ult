a779b9a978992ed6953f675bf7d18d652ffa27ba
mohamedisakr <mohamedisakr@gmail.com>
2022-02-17 12:50:35 +0200

    implementing signin form functionality

---
+++ src/auth/Signin.js
@@ -0,0 +1,90 @@
+import axios from 'axios'
+import React, {useState} from 'react'
+import {toast} from 'react-toastify'
+import Layout from '../core/Layout'
+
+const Signin = () => {
+  const [email, setEmail] = useState('testman1978@hotmail.com')
+  const [password, setPassword] = useState('Password123')
+  const [buttonText, setButtonText] = useState('Submit')
+
+  const {REACT_APP_API_URL} = process.env
+  console.log(`api url : ${REACT_APP_API_URL}`)
+
+  const handleEmailChange = (e) => {
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault()
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setButtonText('Submitting...')
+
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: `${REACT_APP_API_URL}/signin`,
+        data: {email, password},
+      })
+
+      console.log(`Signin data : ${JSON.stringify(res)}`)
+      // save {token, user} to local storage & cookie & redux
+      setEmail('')
+      setPassword('')
+      setButtonText('Submitted')
+      toast.success(`Hello ${res.data.user.name}!`)
+    } catch (err) {
+      //   console.log(`Signin error: ${err.response.data.error}`)
+      console.log(`Signin error: ${JSON.stringify(err.response.data.message)}`)
+      setButtonText('Submit')
+      toast.error(err.response.data.message)
+    }
+  }
+
+  const signinForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <label className="text-muted">Email:</label>
+          <input
+            value={email}
+            onChange={handleEmailChange}
+            type="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="text-muted">Password:</label>
+          <input
+            value={password}
+            onChange={handlePasswordChange}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        <div>
+          <button onClick={handleSubmit} className="btn btn-primary">
+            {buttonText}
+          </button>
+        </div>
+      </form>
+    )
+  }
+  return (
+    <Layout>
+      <div className="col-d-6 offset-md-3">
+        <h1 className="p-5 text-center">Signin</h1>
+        {signinForm()}
+        {/* <pre>{JSON.stringify({name, email, password})}</pre> */}
+      </div>
+    </Layout>
+  )
+}
+
+export default Signin
